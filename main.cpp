34fc4fb1d3c8b733628ee0d626118ae24859a94f
Justin Berkshire <justinwendellberkshire@gmail.com>
2022-08-08 15:40:44 -0500

    investigating i2c communication issue

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@
 #include "pico/stdlib.h"
 #include "pico/binary_info.h"
 #include "hardware/i2c.h"
+ // #include "./registerActions.cpp"
 using namespace std;
 
 //hardware registers
@@ -61,37 +62,52 @@ class KitronikBoard {
         //found this, not sure what it does
         bi_decl(bi_2pins_with_func(8, 9, GPIO_FUNC_I2C));
         //begin proper value writing for initialization
+        uint8_t wakeup[1] = { WAKE_UP };
+        reg_write(READ_READY_ON_DEVICE, wakeup, 1);
         swReset();
         //set up servo pwm settings for the board. 
         //SET servo pwm clock to 20ms this can vary by servo, but 20ms is typical for hobby servos
-        uint8_t pwmbuf[2] = { PWM_REG_ADDRESS, PWM_VALUE_20ms_PULSE };
-        writeDoubleBuffer(pwmbuf);
+        uint8_t pwmbuf[1] = { PWM_VALUE_20ms_PULSE };
+        reg_write(PWM_REG_ADDRESS, pwmbuf, 1);
         // block write outputs
-        disableWritesFromI2cSlave();
-        //come out of sleep
-        uint8_t wakeup[2] = { READ_READY_ON_DEVICE, WAKE_UP };
-        writeDoubleBuffer(wakeup);
-
-
-
+        // disableWritesFromI2cSlave();
     };
-    private: void writeDoubleBuffer(uint8_t buffer[2]) {
-        i2c_write_blocking(i2c0, 108, buffer, 2, true);
-    }
+          int reg_write(const uint8_t reg, uint8_t* buf, const uint8_t nbytes) {
+
+              int num_bytes_read = 0;
+              uint8_t msg[nbytes + 1];
+
+              // Check to make sure caller is sending 1 or more bytes
+              if (nbytes < 1) {
+                  return 0;
+              }
+
+              // Append register address to front of data packet
+              msg[0] = reg;
+              for (int i = 0; i < nbytes; i++) {
+                  msg[i + 1] = buf[i];
+              }
+
+              // Write data to register(s) over I2C
+              i2c_write_blocking(i2c0, I2CAddress, msg, (nbytes + 1), false);
+
+              return num_bytes_read;
+          }
+
     private: void disableWritesFromI2cSlave() {
         uint8_t writeOne[2] = { READ_ADDRESS_ONE, _u(0x00) };
         uint8_t writeTwo[2] = { READ_ADDRESS_TWO, _u(0x00) };
         uint8_t writeThree[2] = { READ_ADDRESS_THREE, _u(0x00) };
         uint8_t writeFour[2] = { READ_ADDRESS_FOUR, _u(0x00) };
-        writeDoubleBuffer(writeOne);
-        writeDoubleBuffer(writeTwo);
-        writeDoubleBuffer(writeThree);
-        writeDoubleBuffer(writeFour);
+        // writeDoubleBuffer(writeOne);
+        // writeDoubleBuffer(writeTwo);
+        // writeDoubleBuffer(writeThree);
+        // writeDoubleBuffer(writeFour);
 
     }
     public: void swReset() {
-        uint8_t buf[1] = { KIT_RESET };
-        i2c_write_blocking(i2c0, 108, buf, 1, true);
+        uint8_t buf[1] = { 0x01 };
+        reg_write(KIT_RESET, buf, 1);
     }
     public: void servoSetAngle(double angle, uint servo) {
         if (angle > sAngleMax) angle = 180;
@@ -100,19 +116,23 @@ class KitronikBoard {
         //Calculate Servo Register
         uint8_t servoRegister = SRV_REG_BASE + (servo * servoRegOffset);
         //Calculate mills to set servo pwm to. this is currently going to use default numbers; dynamic cacalculation examples can be found in https://github.com/jberks02/buttonControlledArm 
-        uint8_t newServoMills = (angle * 2.2755) + 102;
+        uint8_t newServoMills[1] = { (angle * 2.2755) + 102 };
 
-        uint8_t lowBytePWM = newServoMills & 0xFF;
-        uint8_t highBytePWM = (newServoMills >> 8) & 0x01;
-        uint8_t highByteRegister = servoRegister + 1;
+        reg_write(servoRegister, newServoMills, 1);
 
-        uint8_t lowByte[2] = { servoRegister, lowBytePWM };
-        uint8_t highByte[2] = {highByteRegister, highBytePWM};
+        // uint8_t lowBytePWM = newServoMills & 0xFF;
+        // uint8_t highBytePWM = (newServoMills >> 8) & 0x01;
+        // uint8_t highByteRegister = servoRegister + 1;
+
+
+
+        // uint8_t lowByte[2] = { servoRegister, lowBytePWM };
+        // uint8_t highByte[2] = {highByteRegister, highBytePWM};
         // uint8_t lowByte[2] = { servoRegister + 0x01, newServoMills & 0xFF };
 
         // writeDoubleBuffer(lowByte);
-        writeDoubleBuffer(lowByte);
-        writeDoubleBuffer(highByte);
+        // writeDoubleBuffer(lowByte);
+        // writeDoubleBuffer(highByte);
 
     }
 
@@ -122,11 +142,11 @@ int main() {
 
     KitronikBoard kitBoard;
 
-    kitBoard.servoSetAngle(90, 0);
+    kitBoard.servoSetAngle(5, 1);
 
     sleep_ms(500);
 
-    kitBoard.servoSetAngle(180, 0);
+    kitBoard.servoSetAngle(50, 1);
 
     gpio_init(LED);
 
